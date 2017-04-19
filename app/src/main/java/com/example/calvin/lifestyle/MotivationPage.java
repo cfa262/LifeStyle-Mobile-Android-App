cc191b7e6c0510731485d1c3c2ddcebb637ae705
jsuyat <jsuyat@masonlive.gmu.edu>
2017-04-19 19:43:28 -0400

    worked on Motivation for a bit. trying to figure out how to put user input

--- app/src/main/java/com/example/calvin/lifestyle/MotivationPage.java
+++ app/src/main/java/com/example/calvin/lifestyle/MotivationPage.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import java.util.*;
 
 /**
  * Created by Calvin on 3/18/2017.
@@ -11,6 +12,7 @@ import android.view.View;
 
 public class MotivationPage extends Activity {
 
+    ArrayList<String> quotes = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +20,41 @@ public class MotivationPage extends Activity {
 
     }
 
+    public void addQuote(){
+        quotes.add("Add quote here");
+    }
+
+    //@Override
+    public void onWaterClicked(View v){
+        if(v.getId() == R.id.BWater){
+            Intent i = new Intent(MotivationPage.this, Water.class);
+            startActivity(i);
+
+        }
+    }
+
+    public void onSettingsClicked(View s){
+        if(s.getId() == R.id.BSettings){
+            Intent a = new Intent(MotivationPage.this, SettingsPage.class);
+            startActivity(a);
+
+        }
+    }
+
+    public void onProductivityClicked(View s){
+        if(s.getId() == R.id.BProductivity){
+            Intent p = new Intent(MotivationPage.this, ProductivityPage.class);
+            startActivity(p);
+
+        }
+    }
+
     //goes back to Settings page
-    public void onMotivationBackClicked(View s){
+    /*public void onMotivationBackClicked(View s){
         if(s.getId() == R.id.backMotivationButton){
             Intent i = new Intent(MotivationPage.this, IntroPage.class);
             startActivity(i);
 
         }
-    }
+    }*/
 }
