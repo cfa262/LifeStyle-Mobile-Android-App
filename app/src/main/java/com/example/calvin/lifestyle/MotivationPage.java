e6b08d440378cc3e571164546153bd46b74afa5b
jsuyat <jsuyat@masonlive.gmu.edu>
2017-04-23 21:41:48 -0400

    worked on some parts of motivation pages

--- app/src/main/java/com/example/calvin/lifestyle/MotivationPage.java
+++ app/src/main/java/com/example/calvin/lifestyle/MotivationPage.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.*;
 
@@ -12,62 +12,41 @@ import java.util.*;
 
 public class MotivationPage extends Activity {
 
-    private ArrayList<String> quotes = new ArrayList<String>();
+    TextView motivationalQuote;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.motivation_page);
 
-    }
-
-    public void quotes(){
-        this.quotes.add("The harder you try, the better you are");
-        this.quotes.add("Lets get moving!");
-        this.quotes.add("Keep it up!");
-        this.quotes.add("You are going to do great!");
-        this.quotes.add("No sleep till its done.");
+        motivationalQuote = (TextView) findViewById(R.id.quoteGoesHere);
     }
 
     //adds quote to current list of quotes
-    public void addQuote(View s){
+    /*public void addQuote(View s){
         if(s.getID().equals(R.id.addQuote)){
             EditText input = (EditText)findViewById(R.id.waterNumberText);
             String newQuote = input.getText().toString();
             this.quotes.add(newQuote);
         }
-    }
+    }*/
 
-    //@Override
-    public void onWaterClicked(View v){
-        if(v.getId() == R.id.BWater){
-            Intent i = new Intent(MotivationPage.this, Water.class);
-            startActivity(i);
 
-        }
-    }
-
-    public void onSettingsClicked(View s){
-        if(s.getId() == R.id.BSettings){
-            Intent a = new Intent(MotivationPage.this, SettingsPage.class);
-            startActivity(a);
-
-        }
-    }
-
-    public void onProductivityClicked(View s){
-        if(s.getId() == R.id.BProductivity){
-            Intent p = new Intent(MotivationPage.this, ProductivityPage.class);
-            startActivity(p);
-
-        }
-    }
 
     //goes back to Settings page
-    /*public void onMotivationBackClicked(View s){
+    public void onMotivationBackClicked(View s){
         if(s.getId() == R.id.backMotivationButton){
             Intent i = new Intent(MotivationPage.this, IntroPage.class);
             startActivity(i);
 
         }
-    }*/
+    }
+
+    //goes to the Motivation Settings page
+    public void onMotivationSettingsClicked(View s){
+        if(s.getId() == R.id.motivationSettingsButton){
+            Intent i = new Intent(MotivationPage.this, IntroPage.class);
+            startActivity(i);
+        }
+    }
 }
--- app/src/main/java/com/example/calvin/lifestyle/MotivationSettings.java
+++ app/src/main/java/com/example/calvin/lifestyle/MotivationSettings.java
@@ -5,22 +5,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+
+import java.util.*;
 
 
 
 public class MotivationSettings extends Activity {
 
+    private ArrayList<String> quotes = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.motivation_settings);
 
+        //list of motivational quotes
+        Switch switchOne   = (Switch) findViewById(R.id.switch1);
+        Switch switchTwo   = (Switch) findViewById(R.id.switch10);
+        Switch switchThree = (Switch) findViewById(R.id.switch13);
+        Switch switchFour  = (Switch) findViewById(R.id.switch14);
+
+
+
     }
 
-    //goes back to the movitation settings page
+    //goes back to the movitation page page
     public void onMotiSettingsBackClicked(View s){
         if(s.getId() == R.id.motivationSettingsBackButton){
-            Intent i = new Intent(MotivationSettings.this, SettingsPage.class);
+            Intent i = new Intent(MotivationSettings.this, MotivationPage.class);
             startActivity(i);
 
         }
