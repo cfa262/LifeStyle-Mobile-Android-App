75844b342e33dc83acb0c27088fd841c7c8bec31
petris1997 <petris1997@hotmail.com>
2017-04-27 22:19:51 -0400

    Added application usage statistics to productivity page. Added application usage permissions to the AndroidManifest.xml file.

--- app/src/test/java/com/example/calvin/lifestyle/AppTestingCases.java
+++ app/src/test/java/com/example/calvin/lifestyle/AppTestingCases.java
@@ -1,6 +1,25 @@
 package com.example.calvin.lifestyle;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -9,7 +28,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class AppTestingCases {
+public class AppTestingCases extends Activity {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
@@ -42,7 +61,75 @@ public class AppTestingCases {
     @Test
     public void productivity_notification() throws Exception{
         //test if productivity notification is working
+        Switch proswitch = (Switch) findViewById(R.id.proswitch);
+        String indicator= "OFF";
+
+        proswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            public void onCheckedChanged(CompoundButton buttonview, boolean isChecked) {
+
+                if (isChecked) {
+                    indicator="ON";
+                    Toast.makeText(getApplicationContext(), indicator, Toast.LENGTH_LONG).show();
+
+                    assertEquals(indicator, "ON");
+
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.set(Calendar.HOUR_OF_DAY, 16);
+                    calendar.set(Calendar.MINUTE, 41);
+                    calendar.set(Calendar.SECOND, 0);
+                    Intent intent = new Intent(ProductivitySettings.this, NotificationProductivity.class);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(ProductivitySettings.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                    alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+
+                } else {
+                    Toast.makeText(getApplicationContext(), indicator, Toast.LENGTH_LONG).show();
+                    assertEquals(indicator, "OFF");
+                }
+            }
+        });
 
     }
 
+    @Test
+    public void is_data_displayed() throws Exception{
+        //test if data is being displayed
+        UsageStatsManager lUsageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
+        List<UsageStats> lUsageStatsList = lUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, System.currentTimeMillis()- TimeUnit.DAYS.toMillis(1),System.currentTimeMillis()+ TimeUnit.DAYS.toMillis(1));
+
+        assertNotEquals(lUsageStatsList,0);
+    }
+
+    @Test
+    public void usage_time_is_correct() throws Exception{
+        //test if the time of the application is correct
+
+        UsageStatsManager lUsageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
+        List<UsageStats> lUsageStatsList = lUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, System.currentTimeMillis()- TimeUnit.DAYS.toMillis(1),System.currentTimeMillis()+ TimeUnit.DAYS.toMillis(1));
+
+        for (UsageStats lUsageStats:lUsageStatsList) {
+            long time=TimeUnit.MILLISECONDS.toMinutes((lUsageStats.getTotalTimeInForeground()));
+            assertNotEquals(time,0);
+            }
+
+        }
+
+    @Test
+    public void set_time_limit_notification_settings() throws Exception{
+        //test if time limit is being set
+        MyDatabase usage;
+
+        int waterAmount;
+        int currentWaterAmount = 0;
+        TextView waterTextView;
+        EditText editWaterText;
+
+        String waterText = editWaterText.getText().toString();
+        waterAmount = Integer.parseInt(waterText);
+        waterTextView.setText(Integer.toString(waterAmount));
+        WaterProduct product = new WaterProduct(waterTextView.getText().toString());
+        usage.addProduct(product);
+
+        assertNotEquals(currentWaterAmount,0);
 }
