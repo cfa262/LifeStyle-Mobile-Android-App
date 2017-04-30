bf3edc7d4241b454d32bd3fd6e8df23216e11124
jsuyat <jsuyat@masonlive.gmu.edu>
2017-04-30 12:22:36 -0400

    worked on some parts of motivation pages

--- app/src/test/java/com/example/calvin/lifestyle/JedsTestCases.java
+++ app/src/test/java/com/example/calvin/lifestyle/JedsTestCases.java
@@ -9,7 +9,7 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 @RunWith(AndroidJUnit4.class)
-@LargeTest
+
 
 /**
  * Created by Jed on 4/29/2017.
@@ -17,15 +17,44 @@ import android.support.test.runner.AndroidJUnit4;
 
 public class JedsTestCases {
 
-    private String stringToBeShown;
-
     @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MotivationPage.class);
+    public ActivityTestRule<MotivationPage> mActivityRule = new ActivityTestRule<>(MotivationPage.class);
 
-    @Before
-    public void initValidString(){
+    @Test
+    public void youGotThisTextView() throws Exception{
 
+        //checks to see if the TextView reads "You got this!
+        //when it's initially opened
+        onView(withId(R.id.quoteGoesHere))
+                .check(matches("You got this!"));
     }
 
+    @Test
+    public void goToMotivationPage() throws Exception{
 
+        //checks to see if button with ID motivationSettingsButton
+        //is clickable and goes to MotivationSettings
+
+        onView(withId(R.id.motivationSettingsButton))
+                .perform(click())
+                .check(matches(instanceof(MotivationSettings)));
+    }
+
+    @Test
+    public void switchOn() throws Exception{
+        onView(withId(R.id.motivationSettingsButton))
+                .perform(click());
+
+        //checks to see if switch1 is enabled.
+        onView(withId(R.id.switch1))
+                .perform(click())
+                .check(matches(isEnable()));
+
+        onView(withId(R.id.backButton))
+                .perform(click())
+                .check(matches(instanceof(MotivationPage)));
+
+        onView(withId(R.id.quoteGoesHere))
+                .check(matches("The harder you try, the better you are"));
+    }
 }
